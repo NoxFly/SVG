61ed09f04b71a8e0c5b52008d049524066132ac8
NoxFly <dorian.thivolle@gmail.com>
2019-06-13 16:43:52 +0200

    SVG lib

--- svg.js
+++ svg.js
@@ -37,11 +37,11 @@ class SVG {
         let elSVG = document.createElementNS(this.xmlns, "svg");
         document.body.appendChild(elSVG);
 
-        elSVG.setAttribute("class", name);
-        Object.assign(elSVG.style, {
-            width: width,
-            height: height,
-            display: "inline-block"
+        setAttributes(elSVG, {
+            width: this.width+"px",
+            height: this.height+"px",
+            display: "inline-block",
+            class: this.name
         });
     }
 
